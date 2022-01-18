e42fef11dae5b05ec73a7f9809d30a274a51ed8c
mili <m.ahrari77@gmail.com>
2022-01-18 23:51:38 +0330

    controller.py updated

--- controller.py
+++ controller.py
@@ -3,5 +3,5 @@ import views
 
 user = models.User('user')
 
-win = views.UserPanel(user.words, user.add_word, user.add_answer, user.check_repeated, user.learned, user.export_csv)
+win = views.UserPanel(user.words, user.add_word, user.add_answer, user.check_repeated, user.learned, user.export_csv, user.import_csv)
 win.mainloop()
